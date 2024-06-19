76f45471db7760ab7f5e25c018d368ffa6bf1551
fgalego <fgalego@gmail.com>
2024-06-19 09:52:52 +0100

    Info change

--- src/components/Informacao.tsx
+++ src/components/Informacao.tsx
@@ -4,7 +4,7 @@ function Informacao() {
   return (
     <div className="flex items-center justify-center mt-8">
       <p>
-        O Município de Bragança apresenta, neste portal, o Serviço de dados do
+        O Município de Arraiolos apresenta, neste portal, o Serviço de dados do
         arvoredo urbano integrante do domínio público municipal e do domínio
         privado do município e ao património arbóreo pertencente ao Estado.
         <br />
